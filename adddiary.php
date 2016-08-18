ac2bbed1a2aa308c402083573fa1baaeba32ab29
1414504 <a@a.com>
2016-08-18 11:03:41 +0100

    diary

--- adddiary.php
+++ adddiary.php
@@ -32,7 +32,7 @@ if(isset($_POST['save_diary']))
         <script src="bootstrap/js/jquery-1.12.0.min.js" type="text/javascript"></script>
         <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
        <link href="style.css" rel="stylesheet" type="text/css"/>
-        <title>diaryTracker</title>
+        <title>myDiary</title>
     </head>
     <body>
         
