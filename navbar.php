7f28f301a3464a5b5092e5483b906d0dd185c94f
1414504 <a@a.com>
2016-08-18 09:40:30 +0100

    diary

--- navbar.php
+++ navbar.php
@@ -1,22 +1,22 @@
 <div class="navbar">
     <div class="row">
-        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white; width: 100%; text-align: left;" href="showBugs.php">All Bug Items</a></h5></div>
+        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white; width: 100%; text-align: left;" href="showBugs.php">All Diary Items</a></h5></div>
 
     </div>
     <div class="row">
-        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white; width: 100%;text-align: left;" href="show-category-bugs.php?cat=android">Android Bugs</a></h5></div>
+        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white; width: 100%;text-align: left;" href="show-category-bugs.php?cat=android">Work Items</a></h5></div>
 
     </div>
     <div class="row">
-        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white;width: 100%;text-align: left;" href="show-category-bugs.php?cat=iOS">IOS Bugs</a></h5></div>
+        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white;width: 100%;text-align: left;" href="show-category-bugs.php?cat=iOS">University Items</a></h5></div>
 
     </div>
     <div class="row">
-        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white;width: 100%;text-align: left;" href="show-category-bugs.php?cat=windows">Windows Bugs</a></h5></div>
+        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white;width: 100%;text-align: left;" href="show-category-bugs.php?cat=windows">Family Items</a></h5></div>
 
     </div>
     <div class="row">
-        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white;width: 100%;text-align: left;" href="addBugs.php">Insert Bug</a></h5></div>
+        <div class="col-md-12"><h5><a class="btn btn-default" style="background-color: blue; color: white;width: 100%;text-align: left;" href="addBugs.php">Insert a Diary Item</a></h5></div>
 
     </div>
 
