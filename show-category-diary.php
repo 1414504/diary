63b797e89efcda3533211238c99cefb71413b53a
1414504 <a@a.com>
2016-08-18 11:02:46 +0100

    diary

--- show-category-diary.php
+++ show-category-diary.php
@@ -16,7 +16,7 @@ if (isset($_GET['cat'])) {
         <script src="bootstrap/js/jquery-1.12.0.min.js" type="text/javascript"></script>
         <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
         <link href="style.css" rel="stylesheet" type="text/css"/>
-        <title>diaryTracker</title>
+        <title>myDiary</title>
     </head>
     <body>
 
